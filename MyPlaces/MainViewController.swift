4a36e4d248b21a930dd64bd8bf968481fb3b2332
Max Pavlov <max.pavlov711@gmail.com>
2022-02-02 17:49:01 +0300

    Custom Rating Control 2

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -74,6 +74,10 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
     }
     
     // MARK: - Table view delegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
         if editingStyle == .delete {
--- MyPlaces/RatingControl.swift
+++ MyPlaces/RatingControl.swift
@@ -10,7 +10,13 @@ import UIKit
 @IBDesignable class RatingControl: UIStackView {
     
     //    MARK: - Properties
-    private var retingButtons = [UIButton]()
+    var rating  = 0 {
+        didSet {
+            updateButtonSelectionState()
+        }
+    }
+    
+    private var ratingButtons = [UIButton]()
     
     @IBInspectable var starSize: CGSize = CGSize(width: 44.00, height: 44.0) {
         didSet {
@@ -23,8 +29,6 @@ import UIKit
         }
     }
     
-    var rating  = 0
-    
     //    MARK: - Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -36,35 +40,59 @@ import UIKit
         setupButtons()
     }
     //    MARK: - Button Action
-    @objc func retingButtonTapped(button: UIButton) {
-        print("Button pressed 👍")
+    @objc func ratingButtonTapped(button: UIButton) {
+        guard let index = ratingButtons.firstIndex(of: button) else { return }
+        //  Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            rating = 0
+        } else {
+            rating = selectedRating
+        }
     }
     
     //    MARK: - Private Methods
     private func setupButtons() {
         
-        for button in retingButtons {
+        for button in ratingButtons {
             removeArrangedSubview(button)
             button.removeFromSuperview()
         }
         
-        retingButtons.removeAll()
+        ratingButtons.removeAll()
+        
+        // Load button image
+        let bundle = Bundle(for: type(of: self))
+        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named: "emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named: "highlightedStar", in: bundle, compatibleWith: self.traitCollection)
         
         for _ in 0..<starCount {
             // Create the button
             let button = UIButton()
-            button.backgroundColor = .red
+            // Set the button image
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
             // Add contraints
             button.translatesAutoresizingMaskIntoConstraints = false
             button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
             button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
             // Setup the button action
-            button.addTarget(self, action: #selector(retingButtonTapped(button:)), for: .touchUpInside)
+            button.addTarget(self, action: #selector(ratingButtonTapped(button:)), for: .touchUpInside)
             // Add the button to the stack
             addArrangedSubview(button)
             // Add the new button on the rating button array
-            retingButtons.append(button)
+            ratingButtons.append(button)
         }
-        
+        updateButtonSelectionState()
+    }
+    
+    private func updateButtonSelectionState() {
+        for (index, button) in ratingButtons.enumerated() {
+            button.isSelected = index < rating
+        }
     }
 }
